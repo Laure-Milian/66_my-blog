6d1baaf6e61c0b610c1cbce271fa796f8b67fcf6
Laure <laure.milian@gmail.com>
2017-02-01 10:33:38 +0100

    step 1 - affiche les articles de la bdd

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
+<?php 
+define('BASEPATH', __DIR__);
+require BASEPATH . '/vendor/autoload.php';
+ORM::configure('mysql:host=localhost;dbname=my_blog');
+ORM::configure('username', 'root');
+ORM::configure('password', 'simplonco');
+
+$posts = ORM::for_table('posts')->find_many();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Blogblogblog</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<h1>Mon blog</h1>
+	<div class='container'>
+		<h1>Mon blog</h1>
+		<?php foreach ($posts as $post) : ?>
+			<div>
+				<h2><?= $post->title ?></h2>
+				<div><?= $post->content ?></div>
+				<div><?= $post->author ?>, le <?= $post->created_at ?></div>
+			</div>
+		<?php endforeach; ?>
+	</div>
 </body>
 </html>
